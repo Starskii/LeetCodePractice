87215ae9c672e2f7b262c2c75bb1ee7e451688bc
Starskii <jakeylakie@yahoo.com>
2024-12-28 00:21:56 -0800

    made my RemoveDuplicates solution work in O(n) time by removing the slicing comparison

--- RemoveDuplicates.py
+++ RemoveDuplicates.py
@@ -1,17 +1,15 @@
 nums = [0, 0, 1, 2, 2]
 
 def removeDuplicates(nums):
-    unique_count = 0
-    write_ptr = 0
-    read_ptr = 0
+    unique_count = 1
+    write_ptr = 1
+    read_ptr = 1
     while write_ptr < len(nums) and read_ptr < len(nums):
-        if nums[read_ptr] not in nums[0:write_ptr]:
+        if nums[read_ptr] > nums[read_ptr - 1]:
             unique_count += 1
             nums[write_ptr] = nums[read_ptr]
             write_ptr += 1
-            read_ptr += 1
-        else:
-            read_ptr += 1
+        read_ptr += 1
     return unique_count
 
 print(removeDuplicates(nums))
